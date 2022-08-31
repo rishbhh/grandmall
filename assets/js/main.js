57e3e082ab69dddcebc8e30f2d293d2a4fe4f2bc
rishbhh <sevikash04@gmail.com>
2022-08-31 19:48:22 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -244,3 +244,56 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+ScrollReveal({ reset: false });
+
+ScrollReveal().reveal(".show-once", {
+  reset: false
+});
+
+ScrollReveal().reveal(".title", {
+  duration: 1200,
+  origin: "top",
+  distance: "100px",
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  rotate: {
+    x: 20,
+    // z: -10
+  }
+});
+
+ScrollReveal().reveal(".fade-in", {
+  duration: 2000,
+  move: 0
+});
+
+ScrollReveal().reveal(".scaleUp", {
+  duration: 1200,
+  scale: 0.85
+});
+
+ScrollReveal().reveal(".flip", {
+  delay: 500,
+  duration: 2000,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+
+ScrollReveal().reveal(".slide-right", {
+  duration: 3000,
+  origin: "left",
+  distance: "300px",
+  easing: "ease-in-out"
+});
+
+ScrollReveal().reveal(".slide-up", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "100px",
+  easing: "cubic-bezier(.37,.01,.74,1)",
+  opacity: 0.3,
+  scale: 0.5
+});
